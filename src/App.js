bb4840befdc4f48006057ce934c5ffa473614089
wkagebo <kagebo@kth.se>
2023-05-11 19:24:42 +0200

    transactions table + routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import StartPage from './pages/start';
-import { getCurrentBlockHeight, getData, formatData } from './pages/start/data';
+import DetailsPage from './pages/details';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-        <StartPage/>
-        <a
-          className="App-link"
-          href="https://tzkt.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by TzKT API
-        </a>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/details" element={<DetailsPage />}/>
+      </Routes>
+      <a
+        className="App-link"
+        href="https://tzkt.io/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by TzKT API
+      </a>
     </div>
   );
 }
---
+++ src/components/transactions/index.js
@@ -0,0 +1,33 @@
+import styles from './styles.module.css'
+
+const TransactionTable = ({ transactions, loading, clickedBlock }) => {
+  return (
+    <div>
+        {loading ? (
+            <div>Loading...</div>
+        ) : (
+            <>
+                <h1>Transactions for block {clickedBlock}</h1>
+                <table border={1}>
+                    <tr>
+                        <th>Sender</th>
+                        <th>Target</th>
+                        <th>Amount</th>
+                        <th>Status</th>
+                    </tr>
+                    {transactions.map((transaction, i) => (
+                        <tr key={transaction.sender.address}>
+                            <td>{transaction.sender.alias? transaction.sender.alias : transaction.sender.address}</td>
+                            <td> {transaction.target.alias? transaction.target.alias : transaction.target.address} </td>
+                            <td>{transaction.amount}</td>
+                            <td>{transaction.status}</td>
+                        </tr>
+                    ))}
+                </table>
+            </>
+        )}
+	</div>
+  );
+};
+
+export default TransactionTable;
