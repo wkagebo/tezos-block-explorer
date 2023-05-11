41aad18025b97449fc3b6d3df8eb114f9e4a828c
wkagebo <kagebo@kth.se>
2023-05-11 16:15:30 +0200

    lots of fetching

--- src/pages/start/index.js
+++ src/pages/start/index.js
@@ -1,49 +1,62 @@
 import Table from "../../components/table";
 import Pagination from "../../components/pagination";
-import { useEffect, useState } from "react";
-import { formatData, getData, getCurrentBlockHeight } from "./data";
+import React, { useEffect, useState } from "react";
+import { BASE_URL } from "../../apiConfig";
 
 const StartPage = () => {
-	const [tableData, setTableData] = useState({
-		rowData: [],
-		isLoading: false,
-		totalPages: 0, 
-		totalBlocks: 0,
-	});
 	const [currentPage, setCurrentPage] = useState(1);
+
+	// Table data fetch 
+	const [blocks, setBlocks] = useState([]);
+	const [totalBlocks, setTotalBlocks] = useState(0);
+	const [numTransactions, setNumTransactions] = useState([]);
+	const [loading, setLoading] = useState(false);
+
 	useEffect(() => {
-		setTableData((previousState) => ({
-			...previousState,
-			rowData: [],
-			isLoading: true,
-		}));
-		getData(currentPage).then((info) => {
-			const { totalPages, totalBlocks, data } = info; 
-			setTableData({
-				isLoading: false,
-				rowData: formatData(data),
-				totalPages, 
-				totalBlocks: getCurrentBlockHeight().then(data => data[0].level),
-			});
-		});
-	}, [currentPage]); 
-	return (
+		setLoading(true)
+		// this fetches current block height
+		fetch(`${BASE_URL}/blocks?select=level&sort.desc=level&limit=1`)
+			.then(response => response.json())
+			.then(data => setTotalBlocks(data[0]))
+		// this fetches the 15 blocks for the current page
+		fetch(`${BASE_URL}/blocks?select=level,timestamp,proposer&offset.pg=${currentPage}&limit=15&sort.desc=level`)
+		  .then(response => response.json())
+		  .then(data => {
+			setBlocks(data);
+
+			// Create fetch urls for every block in current page
+			let urls = [];
+			for (let i = 0; i < 15; i++) {
+				urls = [...urls, `${BASE_URL}/operations/transactions/count?level.eq=${data[i].level}`];
+			}
+
+			//this fetches all the transaction amounts for the relevant blocks being showcased
+			return Promise.all(urls.map(url => fetch(url)));
+		  })
+		  .then(responses => Promise.all(responses.map(response => response.json())))
+		  .then(data => {
+			setNumTransactions(data)
+		  })
+		  .finally(setLoading(false))
+	  }, [currentPage]);
+	  
+return (
 		<div>
-			<p>Number of blocks: {tableData.totalBlocks || "Loading..."}</p>
-			<div>
-				<Table
-					columns={columns}
-					data={tableData.rowData}
-					isLoading={tableData.isLoading}
-				/>
-			</div>
+			<Table
+				blocks={blocks}
+				numTransactions={numTransactions}
+				loading={loading}
+			/>
 			<Pagination
-				numberOfBlocks={tableData.totalBlocks}
+				numberOfBlocks={totalBlocks}
 				pageChangeHandler={setCurrentPage}
 				blocksPerPage={15}
+				currentPage={currentPage}
 			/>
 		</div>
 	);
 };
 
 export default StartPage
+
+
